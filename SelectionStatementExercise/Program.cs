839c21eb69abb9cd3eae0c69b15bf9223215fb7d
mangosax <mcguerty@att.net>
2023-08-29 10:19:54 -0500

    statements exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 8);
+            int guessNbr = 0;
+
+            Console.WriteLine($"Guess a number between 1 and 8");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == favNumber)
+            {
+                Console.WriteLine($"You guessed it, {favNumber} was it");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"Guess is to high");
+            }
+
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine($"Guess is to low");
+            }
         }
     }
 }
